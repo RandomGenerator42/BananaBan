adb39d849e5e00c5693437f8029df49dd55119cd
RandomGenerator42 <pepe_frog07@yahoo.com>
2017-10-26 23:36:23 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+let discord = require("discord.js")
+let fs = require("fs")
+
+let app = new discord.Client()
+
+let data = JSON.parse(fs.readFileSync("data.json"))
+
+console.log(data)
+
+app.on('ready', () => {
+  console.log(`Logged in as ${app.user.tag}!`);
+});
+
+app.on('message', message => {
+	if(message.member.roles.find("name", "Moderators")&&message.content.startsWith("!mute")){
+
+    message.mentions.users.forEach(event=>{
+      let user = message.guild.members.get(event.id)
+      data.people.push(event.id)
+      user.addRole("337986831352201246")
+      console.log(event.id)
+      let temp = JSON.stringify(data)
+      fs.writeFile('data.json', temp, 'utf8',()=>{})
+    })
+  }
+  
+});
+
+app.on("guildMemberAdd", (member) => {
+  let arr = data.people.filter(event=>event==member.id)
+  if(arr.length!=0){
+    member.addRole("337986831352201246")
+  }
+})
+
+app.login(procces.env.'MzA0MTQ3Mjk1Mjk5OTYwODMy.DHC7Rw.kvjL6yRA4Be9tr6y5aTmzW66hJs')
