a54584b41903f225c6bada11faaca0e9c7b5e137
RandomGenerator42 <pepe_frog07@yahoo.com>
2017-11-25 12:48:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 let discord = require("discord.js")
 let fs = require("fs")
+let randomSentence = require('random-sentence');
 
 
 let app = new discord.Client()
@@ -7,6 +8,7 @@ let app = new discord.Client()
 let data = JSON.parse(fs.readFileSync("data.json"))
 
 console.log(data)
+let first = true
 
 app.on('ready', () => {
   app.user.setGame('with Lurpahi Balls');
@@ -14,9 +16,13 @@ app.on('ready', () => {
 });
 
 app.on('message', message => {
-	if(message.content == "!jaz Zum Boz!"){
-		message.channel.send("On")
-		setInterval(()=>message.channel.send("On"),1000*60*15)
+	if(message.channel.id == '320750032745791489'&&first){
+		message.channel.send(randomSentence({min: 4, max: 9}))
+		setInterval(()=>{
+      message.channel.send(randomSentence({min: 4, max: 9}))},1000*60*(10+Math.floor(Math.random()*10)
+        )
+      )
+    first = false
 	}
 
 	if(message.member.roles.find("name", "Moderators")&&message.content.startsWith("!mute")){
