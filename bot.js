3d48020933fe4f0ad4dcf8b31c08efb4ded81b17
RandomGenerator42 <pepe_frog07@yahoo.com>
2018-04-05 20:10:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,8 @@ app.on('ready', () => {
 });
 
 app.on('message', message => {
-	if(message.author.id=="398563338323492865"||message.author.id=="137238457784336384"||message.channel.id.includes("175964903033667585")) return
-	if(message.content.includes(app.user.id)) { 
+	if(message.author.id=="398563338323492865"||message.author.id=="137238457784336384") return
+	if(message.content.includes(app.user.id)&&message.channel.id.includes("175964903033667585")) { 
 		message.reply("I am currently taking a break from discord")
 		console.log("it works")
 	}
