6e73d70eb1c5836ec6663888211251200dbcea70
RandomGenerator42 <pepe_frog07@yahoo.com>
2018-09-16 19:58:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,6 @@ let discord = require("discord.js")
 
 let app = new discord.Client()
 
-let data = JSON.parse(fs.readFileSync("data.json"))
-
-console.log(data)
-let first = true
-
 app.on('ready', () => {
   app.user.setGame('break from discord');
   console.log(`Logged in as ${app.user.tag}!`);
