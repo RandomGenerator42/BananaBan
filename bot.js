fc2f51b5d3e7eadeb8deaa050ef68e52f0f7e017
RandomGenerator42 <pepe_frog07@yahoo.com>
2018-10-12 20:20:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ let discord = require("discord.js")
 let app = new discord.Client()
 
 app.on('ready', () => {
-  app.user.setActivity('break from discord');
+  app.user.setActivity('depression');
   console.log(`Logged in as ${app.user.tag}!`);
 });
 
